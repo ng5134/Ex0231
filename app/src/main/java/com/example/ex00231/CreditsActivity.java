43f5d524b137a875dade6852a76460256248d818
ng5134 <ng5134@bs.amalnet.k12.il>
2026-05-26 11:52:55 +0200

    AndroidManifest and CreditsActivity decl and credActivity

--- app/src/main/java/com/example/ex00231/CreditsActivity.java
+++ app/src/main/java/com/example/ex00231/CreditsActivity.java
@@ -1,4 +1,37 @@
 package com.example.ex00231;
 
-public class CreditsActivity {
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+
+public class CreditsActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_credits);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.main_menu, menu);
+        return true;
+    }
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+        int id = item.getItemId();
+
+        if (id != R.id.action_credits) {
+            Intent intent = new Intent(this, MainActivity.class);
+            startActivity(intent);
+            finish();
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
 }
