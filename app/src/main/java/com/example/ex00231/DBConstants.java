e038c18b0f16d42a11851e084cdcc799c1d5d939
ng5134 <ng5134@bs.amalnet.k12.il>
2026-05-26 10:14:17 +0200

    firebase and DBconstants

---
+++ app/src/main/java/com/example/ex00231/DBConstants.java
@@ -0,0 +1,13 @@
+package com.example.ex00231;
+
+public class DBConstants {
+    public static final String FIREBASE_URL = "https://ex00231-default-rtdb.firebaseio.com/"; //my firebase url :)
+    public static final String EXPENSES_NODE = "MyExpensesData"; //the name of the main node in the database
+
+    //the names of the fields in the database
+    public static final String KEY_ID = "keyID";
+    public static final String DESCRIPTION = "Description";
+    public static final String AMOUNT = "Amount";
+    public static final String CATEGORY = "Category";
+    public static final String DATE = "Date";
+}
